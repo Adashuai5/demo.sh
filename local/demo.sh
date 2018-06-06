e734aeb3d1246d97780e1d88cce526073342f13e
zhouyuanda <36737838+Adashuai5@users.noreply.github.com>
2018-06-06 23:35:37 +0800

    Update demo.sh

--- local/demo.sh
+++ local/demo.sh
@@ -6,11 +6,11 @@ if [ -d $1 ]; then
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo "<!DOCTYPE>
- 	<title>Hello</title>
+  echo "<!DOCTYPE>\n
+ 	<title>Hello</title>\n
  	<h1>Hi</h1>" >> index.html
   echo "h1{color: red;}" >> css/style.css
-  echo "var string = 'Hello World'
+  echo "var string = 'Hello World'\n
   	alert(strng)" >> js/main.js
   echo 'success'
   exit
